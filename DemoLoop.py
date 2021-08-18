c8d599d7c92b65fc897b37723d79990cd77dcd16
papasmf1 <papasmf1@gmail.com>
2021-08-18 10:50:38 +0900

    반복구문2

--- DemoLoop.py
+++ DemoLoop.py
@@ -6,6 +6,8 @@ for k,v in d.items():
     print(k, v )
 
 #구구단 출력
+#중단점, 중지점(Break Point)
+#디버깅하는 모드면 무조건 중지 
 for x in [2,3,4,5,6]:
     print("---{0}단---".format(x))
     for y in [1,2,3,4,5,6,7,8,9]:

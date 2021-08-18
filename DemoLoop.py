89074c35112057b668e185667163e5c2d9ed0e74
papasmf1 <papasmf1@gmail.com>
2021-08-18 10:26:15 +0900

    반복구문

---
+++ DemoLoop.py
@@ -0,0 +1,14 @@
+# DemoLoop.py
+
+d = {"apple":100, "orange":200, "kiwi":300}
+
+for k,v in d.items():
+    print(k, v )
+
+#구구단 출력
+for x in [2,3,4,5,6]:
+    print("---{0}단---".format(x))
+    for y in [1,2,3,4,5,6,7,8,9]:
+        print("{0} * {1} = {2}".format(x, y, x*y))
+
+

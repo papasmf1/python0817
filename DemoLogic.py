c52fb776b78f7eb3393d1fadc0d1b733099d6695
papasmf1 <papasmf1@gmail.com>
2021-08-18 09:32:06 +0900

    2장 완료

--- DemoLogic.py
+++ DemoLogic.py
@@ -21,3 +21,32 @@ print( bool(None) )
 print( bool([]) )
 print( bool([1,2,3]) )
 
+
+#얕은 복사(참조만 전달)
+a = [1,2,3]
+b = a 
+a[0] = 38
+print( a )
+print( b )
+print( id(a) )
+print( id(b) )
+
+#깊은 복사는 원본과 복사본을 따로 만들기
+a = [10,20,30]
+b = a[:]
+a[0] = 11 
+print( a )
+print( b )
+print(id(a))
+print(id(b))
+
+#리스트가 아닌 경우는 copy모듈 사용
+import copy 
+a = [100, 200, 300]
+b = copy.deepcopy(a)
+a[0] = 101 
+print( a )
+print( b )
+print(id(a))
+print(id(b))
+

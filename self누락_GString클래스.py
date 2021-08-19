a2f9df5d7872c92c967773c3c1945b397791dfbd
papasmf1 <papasmf1@gmail.com>
2021-08-19 11:41:26 +0900

    클래스 연습 주석 추가

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,17 @@
+#전역변수 
 str = "Not Class Member"
+# 파이썬은 모호한 것 보다는 명확한 것이 좋다~~ 
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수 
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #약간의 실수
+        print(self.str)
 
+#인스턴스 생성
 g = GString()
 g.set("First Message")
 g.print()
--- 메모리관리.py
+++ 메모리관리.py
@@ -1,12 +1,16 @@
 # 메모리관리.py 
 
 class MyClass:
+    #생성자 메서드(초기화 담당)
     def __init__(self, value):
         self.value = value 
         print("Instance is created! value=", value)
+    #소멸자 메서드(유명무실)
     def __del__(self):
         print("Instance is deleted!")
 
 d = MyClass(5)
-del d 
+#참조 카운트(1->0) 스스로 소멸 
+#del d 
 
+print("전체 코드 실행 종료")

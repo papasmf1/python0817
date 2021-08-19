9ede1acc2959e16bd8d95deebce6516e71ae0948
papasmf1 <papasmf1@gmail.com>
2021-08-19 13:26:51 +0900

    클래스 주석 추가2

--- BankAccount.py
+++ BankAccount.py
@@ -2,21 +2,32 @@
 
 #은행의 계정을 표현한 클래스 
 class BankAccount:
+    #생성자 메서드 
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #인스턴스의 멤버 변수 초기화 
+        #내부에 멤버 숨김(__)
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
+    #입금
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
+    #출금
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    #문자열 리턴(object에 정의된 약속된 메서드) 
     def __str__(self):
-        return "{0}, {1}, {2}".format(self.id, 
-        self.name, self.balance)
+        return "{0}, {1}, {2}".format(self.__id, 
+        self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
+#외부에서 접근 
+#print(account1.__balance) 
+#이름 변경(백도어)
+print(account1._BankAccount__balance) 
 print(account1)
 
 
--- 정적메서드.py
+++ 정적메서드.py
@@ -1,10 +1,17 @@
 # 정적메서드.py
 class MyCalc(object):
+    #클래스에서 호출(데코레이터)
     @staticmethod
     def my_add(x,y):
         return x+y
+    #인스턴스에서 호출
+    def methodA(self):
+        print("인스턴스에서 호출")
+
 
 #클래스에서 직접 호출한다.
 a = MyCalc.my_add(5,7)
 print(a)
 
+calc = MyCalc()
+calc.methodA() 

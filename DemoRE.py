3e7b3b507339e350774b87525d18cc2afb22e9ef
papasmf1 <papasmf1@gmail.com>
2021-08-20 14:06:47 +0900

    정규표현식3

--- DemoRE.py
+++ DemoRE.py
@@ -20,3 +20,12 @@ result = re.match("[0-9]*th", "  35th")
 result = re.search("[0-9]*th", "  35th")
 print( result )
 print( result.group() )
+
+#단어나 우편번호 검색 
+print( bool(re.match("apple", "this is apple")) )
+print( bool(re.search("apple", "this is apple")) )
+print("---우편번호---")
+print( bool(re.search("\d{5}", "우리동네는 52300")) )
+print( bool(re.search("\d{4}", "올해는 2021년")) )
+
+

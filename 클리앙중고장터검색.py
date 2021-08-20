0a7aff07ec9bfc5e2122519789a2ab6b88e41a47
papasmf1 <papasmf1@gmail.com>
2021-08-20 16:49:43 +0900

    클리앙2

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re 
 
+#웹봇(웹로봇) 자동화 프로그램(창과 방패)
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
@@ -12,7 +13,7 @@ for n in range(0,10):
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
+
         data = urllib.request.urlopen(req).read()
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
@@ -25,9 +26,9 @@ for n in range(0,10):
         for item in list:
                 try:
                         title = item.text.strip()  
-                        print(title)
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
+                        #print(title)
+                        if (re.search('애플', title)):
+                                print(title.strip())
                 except:
                         pass
         

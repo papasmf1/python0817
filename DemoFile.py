89fe450e6ee419d6ff7ca9ac0ca8896b19fd9a85
papasmf1 <papasmf1@gmail.com>
2021-08-20 10:30:07 +0900

    파일연습1

---
+++ DemoFile.py
@@ -0,0 +1,25 @@
+# DemoFile.py 
+
+for x in range(1,6):
+    print(x,"*",x,"=",x*x)
+
+print("---오른쪽 정렬---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).rjust(3) )
+
+print("---0으로 채우기---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).zfill(3) )
+
+#서식을 문자로 지정
+print("{0:x}".format(10) )
+print("{0:b}".format(10) )
+#화폐를 출력
+print("{0:,}".format(15000000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
+
+
+
+
